5e1fa3edd454e617c0ce17e697027bbedd032bd3
Bordman-ger <72354455+Bordman-ger@users.noreply.github.com>
2020-10-24 12:24:01 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"nanodmx2 adapter settings": {
+		"en": "Adapter settings for nanodmx2",
+		"de": "Adaptereinstellungen für nanodmx2",
+		"ru": "Настройки адаптера для nanodmx2",
+		"pt": "Configurações do adaptador para nanodmx2",
+		"nl": "Adapterinstellingen voor nanodmx2",
+		"fr": "Paramètres d'adaptateur pour nanodmx2",
+		"it": "Impostazioni dell'adattatore per nanodmx2",
+		"es": "Ajustes del adaptador para nanodmx2",
+		"pl": "Ustawienia adaptera dla nanodmx2",
+		"zh-cn": "nanodmx2的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
