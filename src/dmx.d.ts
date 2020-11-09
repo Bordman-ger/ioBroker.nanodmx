84a6a172ced47d036e3ecf9ea643374af4cdeab5
Bordman-ger <72354455+Bordman-ger@users.noreply.github.com>
2020-11-09 13:36:30 +0100

    init

--- src/dmx.d.ts
+++ src/dmx.d.ts
@@ -1,12 +1,12 @@
 declare module 'dmx';
-declare class DMX {
-    constructor (options:any);
-        const opt = options || {};
-        const devices = opt.devices || {};
-        // universes = {};
-        // drivers = {};
-        // animation = require('./anim');
+// declare class DMX {
+//     constructor (options:any);
+//         const opt = options || {};
+//         const devices = opt.devices || {};
+//         // universes = {};
+//         // drivers = {};
+//         // animation = require('./anim');
 
 
-  registerDriver(name:string, module:any): void;
-} 
+//   registerDriver(name:string, module:any): void;
+// } 
